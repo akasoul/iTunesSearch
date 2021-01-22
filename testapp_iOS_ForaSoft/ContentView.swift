4abe864866ceb1401eb7388d88bc3eae5a6724af
akasoul <antonvoloshuk@gmail.com>
2021-01-22 20:41:03 +0300

    Initial Commit

---
+++ testapp_iOS_ForaSoft/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testapp_iOS_ForaSoft
+//
+//  Created by Anton Voloshuk on 22.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
